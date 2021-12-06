7587a922d036aa746bc7e9a9a47fcf1bfa48a751
Tutorials-Git <ankireddy905292@gmail.com>
2021-12-06 18:17:40 +0530

    weekday

--- src/com/hotel/HotelReservation.java
+++ src/com/hotel/HotelReservation.java
@@ -4,8 +4,10 @@ public class HotelReservation {
     public static void main(String[] args) {
 
         HotelReservationMethods hotelReservationMethods = new HotelReservationMethods();
-        HotelReservationMethods.viewHotels();
-        hotelReservationMethods.getMinPriceHotel();
+        hotelReservationMethods.addHotel("LakeWood", 110, 90);
+        hotelReservationMethods.addHotel("BridgeWood", 150, 50);
+        hotelReservationMethods.addHotel("RidgeWood", 220, 150);
+        hotelReservationMethods.getHotelDetails();
         System.out.println("Welcome to solve hotel Reservation System");
     }
 }
--- src/com/hotel/HotelReservationMethods.java
+++ src/com/hotel/HotelReservationMethods.java
@@ -2,68 +2,58 @@ package com.hotel;
 
 import java.util.*;
 class Hotel{
-    public String hotelName;
 
     public String getHotelName() {
         return hotelName;
     }
+    public Hotel(){};
 
     public void setHotelName(String hotelName) {
         this.hotelName = hotelName;
     }
 
-    public int getHotelPrice() {
-        return hotelPrice;
+    public int getHotelPriceWeekDays() {
+        return hotelPriceWeekDays;
     }
 
-    public void setHotelPrice(int hotelPrice) {
-        this.hotelPrice = hotelPrice;
+    public void setHotelPriceWeekDays(int hotelPriceWeekDays) {
+        this.hotelPriceWeekDays = hotelPriceWeekDays;
     }
 
-    public  int hotelPrice;
-
-    public Hotel(String hotelName, int hotelPrice) {
-        this.hotelName = hotelName;
-        this.hotelPrice = hotelPrice;
+    public int getHotelPriceWeekEnds() {
+        return hotelPriceWeekEnds;
     }
 
-    public String toString(){
-        return "Hotel Name: "+hotelName +"Hotel Price: "+hotelPrice;
+    public void setHotelPriceWeekEnds(int hotelPriceWeekEnds) {
+        this.hotelPriceWeekEnds = hotelPriceWeekEnds;
     }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelName='" + hotelName + '\'' +
+                ", hotelPriceWeekDays=" + hotelPriceWeekDays +
+                ", hotelPriceWeekEnds=" + hotelPriceWeekEnds +
+                '}';
+    }
+
+    private String hotelName;
+    private int hotelPriceWeekDays;
+    private int hotelPriceWeekEnds;
+
 }
 
-public class HotelReservationMethods {
-    public static ArrayList<Hotel> hotelDetails;
 
-    public HotelReservationMethods() {
-        hotelDetails = new ArrayList<>();
-    }
+public class HotelReservationMethods extends Hotel {
+    public ArrayList<Hotel> hotelDetails = new ArrayList<Hotel>();
 
-    public static void addHotel(String hotelName, Integer hotelPrice) {
-        Hotel adder = new Hotel(hotelName, hotelPrice);
+    public void addHotel(String hotelName, int hotelPriceWeekDays, int hotelPriceWeekEnds) {
+        Hotel adder = new Hotel("HotelName",hotelPriceWeekDays,hotelPriceWeekEnds);
         hotelDetails.add(adder);
     }
 
-    public void getMinPriceHotel(){
-        Hotel min =  hotelDetails.stream().min(Comparator.comparing(Hotel::getHotelPrice)).orElseThrow();
-        System.out.println(min);
-    }
-
-    public static void getHotelDetails() {
-        String hotelName1 = "LakeWood";
-        String hotelName2 = "BridgeWood";
-        String hotelName3 = "RidgeWood";
-        int lackWoodPrice1 = 100;
-        int bridgeWoodPrice1 = 160;
-        int ridgeWoodPrice1 = 220;
-
-        addHotel(hotelName1, lackWoodPrice1);
-        addHotel(hotelName2, bridgeWoodPrice1);
-        addHotel(hotelName3, ridgeWoodPrice1);
-    }
-
-    public static void viewHotels() {
-        getHotelDetails();
+    public void getHotelDetails(){
         System.out.println(hotelDetails);
     }
+
 }
